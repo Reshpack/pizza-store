b55d0438b4e991120b8cd5571bb792b2d5aa2425
Suresh Packiry <sureshpackiry73@gmail.com>
2024-09-24 23:50:19 +1000

    Add: Functionality

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,22 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
+
 function AppLayout() {
-  return <div></div>;
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
